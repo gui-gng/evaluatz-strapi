97ff8f4c843c0e80e7c032092d4756598911d5df
Guilherme Nazareth <GNG@Guilhermes-MacBook-Pro.local>
2020-06-18 00:04:22 -0300

    update port

--- config/server.js
+++ config/server.js
@@ -1,4 +1,4 @@
 module.exports = ({ env }) => ({
   host: env('HOST', `${process.env.HOST}`),
-  port: env.int('PORT', process.env.PORT || 1337),
+  port: env.int('PORT', `${process.env.PORT}`),
 });
